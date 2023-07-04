edea58e7a00b8e721d57dbafa29d3efcf23bea4a
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-07-04 00:16:09 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,27 +1,34 @@
-const secret = "QvZQo1LopQAsRue64c2P4E";
+const url = 'https://factorio.zone/api/user/login'; // URL de destino
+const secret = "fijahL0GGv3yC6IhBlBtDV";
 const constructorSend =
-  "userToken=LbKJWQpvqPkfqFSWLAZ67Vp3&" +
-  "visitSecret=" +
+  "userToken=LbKJWQpvqPkfqFSWLAZ67Vp3" +
+  "&visitSecret=" +
   secret +
   "&reconnected=" +
   false +
   "&script=" +
   "https%3A%2F%2Ffactorio.zone%2Fcache%2Fmain.988ed863ea52a416157f.js"
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    "Content-Type": "Accept,*",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Content-Type": "Access-Control-Allow-Origin",
+    "Content-Type": "Allow-Origin-Header",
+    // Se necessário, adicione outros cabeçalhos aqui
+  },
+  body: JSON.stringify(constructorSend)
+};
 
-localStorage.setItem("key","QvZQo1LopQAsRue64c2P4E");
-fetch("https://factorio.zone/api/user/login", {
-  method: "POST",
-  headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  body: constructorSend,
-})
+localStorage.setItem("key",secret);
+fetch(url, requestOptions)
   .then((response) => {
-    if (response.statusCode == 200) {
+    if (response.status == 200) {
       window.location.href = "/terminal";
       alert(response.responseMessage);
     } else {
-      response.text().then((errorMessage) => {
-        alert(errorMessage);
-      });
+        window.location.href = "/Server";
     }
   })
   .catch((error) => console.error(error));
